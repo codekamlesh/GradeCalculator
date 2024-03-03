2712c0ef561f3d722f7faac6d7686726041cf632
codekamlesh <142514652+codekamlesh@users.noreply.github.com>
2024-03-03 16:52:39 +0530

    Calculator.java

---
+++ code/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public int calculateTotalMarks(int[] marks) {
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        return totalMarks;
+    }
+
+    public double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+
+    public char calculateGrade(double percentage) {
+        if (percentage >= 90) {
+            return 'A';
+        } else if (percentage >= 80) {
+            return 'B';
+        } else if (percentage >= 70) {
+            return 'C';
+        } else if (percentage >= 60) {
+            return 'D';
+        } else if (percentage >= 50) {
+            return 'E';
+        } else {
+            return 'F';
+        }
+    }
+}
